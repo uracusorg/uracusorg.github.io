4c39016300bf579dbbe6bc6d588dd9a6a97b7627
uracus <uracus.nlp@gmail.com>
2015-10-29 00:51:23 +0530

    tania birthday page

--- _site/data/js/imgChange.js
+++ _site/data/js/imgChange.js
@@ -12,3 +12,59 @@ function imgChangeSketch(div){
     link=link.replace(name+"-o",name);
     $(img).prop("src",link);
 }
+
+function changeImgLeft(){
+    var img=$(".center-img"),//selects centered image
+        child=img.children(),//selects image of centered element
+        left=$(".left-img"),//selects left image div
+        right=$(".right-img"),//selects right elements div
+        right_child=right.children(),//selects right elements image
+        right_data=right.data("desc"),//takes data from right element
+        desc=$("#personal-desc"),//selects description element
+        link=right_child.prop("src"),
+        name=right_child.data("name"),
+        link2=child.prop("src"),
+        name2=child.data("name");
+    img.removeClass("center-img");//remove centering class from currently centered imgae
+    child.removeClass("center-img-transform");//remove the transformation from image
+    img.addClass("left-img");//adds left class to it
+    link2=link2.replace(name2+"-o",name2);
+    child.prop("src",link2);
+    left.removeClass("left-img");//remove left class from left image
+    left.addClass("right-img");//add right class to left image
+    right.removeClass("right-img");//remove left class from right image
+    right.addClass("center-img");//add centering class to rightt image
+    right_child.addClass("center-img-transform");//add centering transformation to right image
+    link=link.replace(name,name+"-o");
+    right_child.prop("src",link);
+    desc.empty();//clears content
+    desc.append(right_data);//change the description
+}
+
+function changeImgRight(){
+    var img=$(".center-img"),//selects centered image
+        child=img.children(),//selects image of centered element
+        left=$(".left-img"),//selects left image div
+        right=$(".right-img"),//selects left elements div
+        left_child=left.children(),//selects left elements image
+        left_data=left.data("desc"),//takes data from right element
+        desc=$("#personal-desc"),//selects description element
+        link=left_child.prop("src"),
+        name=left_child.data("name"),
+        link2=child.prop("src"),
+        name2=child.data("name");
+    img.removeClass("center-img");//remove centering class from currently centered imgae
+    child.removeClass("center-img-transform");//remove the transformation from image
+    img.addClass("right-img");//adds left class to it
+    link2=link2.replace(name2+"-o",name2);
+    child.prop("src",link2);
+    left.removeClass("left-img");//remove left class from left image
+    left.addClass("center-img");//add right class to left image
+    left_child.addClass("center-img-transform");//add centering transformation to right image
+    right.removeClass("right-img");//remove left class from right image
+    right.addClass("left-img");//add centering class to right image
+    link=link.replace(name,name+"-o");
+    left_child.prop("src",link);
+    desc.empty();//clears content
+    desc.append(left_data);//change the description
+}
---
+++ happy.md
@@ -0,0 +1,45 @@
+---
+layout: default_home
+title: Happy Bday Pagal
+---
+<div  class="container-fluid" style="width: 66%;min-width: 860px;">
+<div class="container-fluid" style="margin-top: 71px;"><span>To<br>Tania<br> sub: idiot its your bday what would have you expected<br>
+on today's occasion we wish you a verrrrrrrrrrrrrrrrry happy bday.<br> may god bless you. and bless you with some brain.<br>
+jokes apart but seriously you have gone nuts,<br> go sell them and buy a gift for yourself from the money collected. :) <br>
+Thanking you.<br>
+the only person you may be thinking of to murder after reading this.
+</span><img style="margin-top: -159px; margin-left: 100px;" src="/data/img/MomentCam_20151028_232823.jpg"></div><br>
+
+<div class="container-fluid"style="width: 568px;"> 
+<img style="margin-top: -247px;margin-right: 132px;" src="/data/img/MomentCam_20151028_232946.jpg">
+<span style="display: inline-block;">on the occasion of your<br>
+21st bday, kanjoosa party<br>
+dedi....<br>
+apke janmdin ke subh mauke<br>
+pr hamne kuch panktian likhi <br>
+hai, gaur farmaiyega. <br>
+you are last ray of light,<br>
+during darkness.<br>
+last drop of water,<br> 
+during drought.<br>
+last byte of food, <br>
+during famine. <br>
+But 1st person sleeping, <br>
+when everyone is awake.<br></span>
+</div>
+<br>
+<div  class="container-fluid"> <span>Irrespective of all the facts and figures<br> and records and whatever...<br>
+you are a true fried,<br> oops friend,<br>
+our true saviour- <br>when it come to assignments, tutorials, practicals, aur whatever<br>
+thanks for being so patient to read uptill here. <br>
+and ek baat hmesha yaad rakhna,<br>
+hmesha yaad rakhna ki, ki... ki...<br>
+aaj tera bday hai,<br> aur bday wale din party dete hai.<br>
+aur han gift......<br><br><br>
+wo to bhul hi jaa,<br> dimag mai aise galt vichar ane bhi ni chahiye.<br>
+aur hum tere ko sbse pehle wish krne wale hai,<br> exact 12:01 pr serve complete hua hai.</span>
+<img style="margin-top: -432px;margin-left: 226px;" src="/data/img/MomentCam_20151028_233012.jpg">
+</div>
+
+
+</div>
